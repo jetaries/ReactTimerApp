3cb8851c8e2494b354098bf6e7e50b2b9d1d32ff
Gordon Chen <jetaries@gmail.com>
2017-08-17 22:11:18 +0800

    add navigation

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -9,7 +9,7 @@ var Navigation = React.createClass({
 					<ul className="menu">
 						<li className="menu-text">React Timer App</li>
 						<li><IndexLink to="/" activeClassName="active-link">Timer</IndexLink></li>
-						<li><Link to="/about" activeClassName="active-link">Countdown</Link></li>
+						<li><Link to="/countdown" activeClassName="active-link">Countdown</Link></li>
 					</ul>
 				</div>
 				<div className="top-bar-right">
